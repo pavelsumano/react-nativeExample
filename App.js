04f8aca47b71fcb686c7dad3a557edc0582a0694
pavel <sumanoop@gmail.com>
2019-09-29 13:04:47 -0500

    Fetch feature

--- App.js
+++ App.js
@@ -4,9 +4,13 @@ import { Text } from 'react-native';
 import Home from './src/screens/containers/home';
 import Header from './src/sections/components/header';
 import SuggestionList from './src/videos/containers/suggestionList';
+import API from './utils/api';
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentDidMount() {
+    API.getSuggestion(10);
+  }
   render() {
     return (
       <Home>
